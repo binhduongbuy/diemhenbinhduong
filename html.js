456d980c03e0c9a02823df9dc400d7583218e974
Truong Dung <binhduongbuy@gmail.com>
2018-04-11 23:36:16 +0700

    1

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-here',
+    google_ad_client: 'ca-pub-6649118088883994',
     enable_page_level_ads: true
   });
   `;
