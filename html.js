d40fdc30b76f20e7672994070211485177d460cc
Truong Dung <34378419+binhduongbuy@users.noreply.github.com>
2018-07-01 06:59:41 +0700

    1

--- html.js
+++ html.js
@@ -70,7 +70,7 @@ function buildGoogleAnalyticsSetup() {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-6625905557664143',
+    google_ad_client: 'ca-pub-6942577674257563',
     enable_page_level_ads: true
   });
   `;
